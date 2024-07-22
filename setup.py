89c463cfcf0f318df9941b614ad60be48a662faf
TalalHabib123 <talalhabib101@gmail.com>
2024-07-22 13:28:17 +0500

    Version 1.0.2 Published

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('PYPIREADME.md', 'r') as f:
 
 setup(
     name='document_forger',
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(),
     install_requires=[
         'opencv-python>=4.5.1', 
